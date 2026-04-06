4c8d478a59e0f15013d23673e27f2d5d57ce4ef2
Mansi Jadhav <264869944+mansijadhav-16@users.noreply.github.com>
2026-04-07 03:21:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'mansi'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'mansi'
+    MYSQL_PASSWORD = 'yourmysqlpassword'
     MYSQL_DB = 'academic_performance'
     WTF_CSRF_ENABLED = True
-    EMAIL_USER = 'vikasvishwakarma10294@gmail.com'
-    EMAIL_PASS = 'kdok hrve hbya hxly'
+    EMAIL_USER = 'yourgmailaddress@gmail.com'
+    EMAIL_PASS = 'your gmail account app password'
